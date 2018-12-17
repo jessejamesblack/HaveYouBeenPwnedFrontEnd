fb4bc28d9048a91fbff21026b1bf687f5ce3dc33
jessejames black <jessejames.black@infsys.com>
2018-12-17 11:47:28 -0500

    scrollbar auto scroll and find me button

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ChatService, Message, Content } from '../../chat.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from "@angular/core";
+import { ChatService, Message, Content } from "../../chat.service";
+import { Observable } from "rxjs";
 import { scan } from "rxjs/operators";
-
-
 
 @Component({
   selector: "chat-dialog",
   templateUrl: "./chat-dialog.component.html",
   styleUrls: ["./chat-dialog.component.css"]
 })
-export class ChatDialogComponent implements OnInit {
+export class ChatDialogComponent implements OnInit, AfterViewChecked {
+  @ViewChild('target') elemRef: ElementRef;
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
   messages: Observable<Message[]>;
   formValue: string;
-  locations: Content[]=[];
+  locations: Content[] = [];
 
   constructor(public chat: ChatService) {}
 
@@ -22,11 +22,28 @@ export class ChatDialogComponent implements OnInit {
     this.messages = this.chat.conversation
       .asObservable()
       .pipe(scan((acc, val) => acc.concat(val)));
-    this.locations = this.chat.locationsFound;
+    this.locations = this.chat.locationsFound;  
+    this.scroll();
   }
-
+  ngAfterViewChecked() {        
+    this.scroll();        
+} 
   sendMessage() {
     this.chat.converse(this.formValue);
     this.formValue = "";
+    this.scroll();
+  }
+  storesNearMe() {
+    this.formValue = "find stores near me";
+    this.chat.converse(this.formValue);
+    this.formValue = "";
+    this.scroll();
+  }
+  scroll(){
+    // console.log(this.elemRef.nativeElement.innerHTML);
+    // this.elemRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+  } catch(err) { } 
   }
 }
