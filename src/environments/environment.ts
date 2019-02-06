f1aba461bd049667f6fcfd61a111af412501bcde
jessejamesblack <jessejamesblack@optonline.net>
2019-02-05 21:45:58 -0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  
+
   dialogflow: {
-    angularBot: '8339a6c31e96400694a796506fc96331  '
+    angularBot: "bbf57a9f65784351aa4b448350d1b92f"
   }
 };
 
